e2c961c857a0ede8e3f5670851847a5374f0b2b8
Nevio Vesic <nevio.vesic@gmail.com>
2014-07-19 00:55:01 +0200

    Cleaning out __xyz__ for all files

--- phonedb/about.py
+++ phonedb/about.py
@@ -1,7 +1,13 @@
 # -*- coding: UTF-8 -*-
 
-from phonedb.about import __author__, __copyright__, __credits__, __license__, __version__, __maintainer__, \
-    __email__, __status__
+__author__ = "Nevio Vesic"
+__copyright__ = "Copyright 2014, Nevio Vesic"
+__credits__ = []
+__license__ = "GPL v3"
+__version__ = "0.0.1"
+__maintainer__ = "Nevio Vesic"
+__email__ = "nevio.vesic@gmail.com"
+__status__ = "Prototype"
 
 # I know, this is like stupid hack but I really do not want to copy myself twice!
 PACKAGE_AUTHOR  = __author__
